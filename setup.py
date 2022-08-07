f27241ae013d944b52b672ff3fd5efe50ec6d936
Sathvik-Adimulam <107644899+Sathvik-Adimulam@users.noreply.github.com>
2022-08-06 19:23:52 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,8 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],                                      # Information to filter the project on PyPi website
-    python_requires='>=3.9',             # Name of the python package
+    python_requires='>=3.9',      
+    py_modules=["ytmusicplayer"]# Name of the python package
     package_dir={'':'F:\ytmusicplayer'},     # Directory of the source code of the package
     install_requires=["pytube==12.1.0",  "moviepy==1.0.3", "requests==2.28.1"]                     # Install other dependencies if any
 )
